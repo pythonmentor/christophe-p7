aeabac8595bab3aaac4eede8c340516e14e6578a
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-07-27 21:58:50 +0200

    add config Constant

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-die"
     BOT_CREATOR = "Christophe Dupin"
     BASE_LINE = "Bienvenue sur Grandpybot Application"
-    GITHUB = ""
+    GITHUB = "https://github.com/Christophe-Dupin/P7GrandPyBot"
     LINKEDIN = "https://www.linkedin.com/in/christophedupin/"
 
