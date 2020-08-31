fb2dba312212be1d6c594b10512f454f9241df4c
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-08-31 15:16:00 +0200

    finish parser class

--- app/parser.py
+++ app/parser.py
@@ -1,25 +1,55 @@
 """Module to manage the parsing of the input sentence."""
+import re
+import unicodedata
+
 from app.config import STOP_WORDS
 
 
 class Parser:
-    """[summary]."""
+    """Class allow to isolate keyword to send to the google map api."""
 
     def __init__(self, sentence):
-        """[summary].
+        """Construct the parser instance.
 
-        :param sentence: [description]
-        :type sentence: [type]
+        :param sentence: input from user
+        :type sentence: [str]
         """
         self.sentence = sentence
 
-    def remove_stop_words(self):
-        return self.sentence.strip()
+    def remove_accent(self):
+        """Remove all accent from the sentence.
 
+        :return: [sentence without accent]
+        :rtype: [str]
+        """
+        try:
+            self.sentence = unicode(text, "utf-8")
+        except NameError:  # unicode is a default on python 3
+            pass
+        self.sentence = (
+            unicodedata.normalize("NFD", self.sentence)
+            .encode("ascii", "ignore")
+            .decode("utf-8")
+        )
+        return str(self.sentence)
 
-if __name__ == "__main__":
-    a = Parser(
-        "salut granpy pourrais tu m indiquer où se situe le musée d art et d histoire de fribourgs il te plait"
-    )
-    print(a.remove_stop_words())
+    def remove_stop_words(self, sentence):
+        """Keep only usefull keyword remove all th stop words.
 
+        :param sentence: sentence to remove stop word
+        :type sentence: [str]
+        :return: [List with only keyword]
+        :rtype: [lst]
+        """
+        self.sentence = re.sub(
+            r"[.!,;?\"\'<>./?@#$%^&*_~]", " ", self.sentence
+        ).split()
+        return ",".join([i for i in self.sentence if i not in STOP_WORDS])
+
+    def process(self):
+        """Methode to run the parsing methode.
+
+        :return: return a liste with only keyword
+        :rtype: [List]
+        """
+        return self.remove_stop_words(self.remove_accent())
