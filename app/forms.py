fe2864eb84334eabeebf2b2d6de2d8ddd796df76
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-09-02 10:14:51 +0200

    remove module

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField, validators
-
-
-class Form(FlaskForm):
-    message = TextAreaField("Votre message")
-    submit = SubmitField("Envoyer")
