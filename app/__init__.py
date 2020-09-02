ea2cdaf93dacb0fb136c33b3c0b226809a50ad01
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-09-02 10:15:14 +0200

    remove unused call to flask module

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
-from config import Config
-from flask_bootstrap import Bootstrap
 
 my_app = Flask(__name__)
-my_app.config.from_object(Config)
 
 from app import routes
 
-boostrap = Bootstrap(my_app)
