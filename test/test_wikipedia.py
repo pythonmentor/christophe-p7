f5d5b9b532281a3f36e88816b9c2f9ddae899f07
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-09-02 10:13:43 +0200

    testing template for wikipedia call API

---
+++ test/test_wikipedia.py
@@ -0,0 +1,14 @@
+"""Module testing the wikipedia class."""
+from app.wikipedia import Wikipedia
+
+import requests
+
+
+class TestWikipedia:
+    """Testing mediaWiki api call."""
+
+    def test_get_article_id(self, monkeypatch):
+        result = {}
+
+    def test_get_article(self, monkeypatch):
+        pass
