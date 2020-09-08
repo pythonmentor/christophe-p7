e421433dc68d84a997773dd28c4b89a8a83db404
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-09-08 20:11:15 +0200

    add new routes

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,33 @@
 """Module to manage the parsing of the input sentence."""
-from flask import render_template
+from app.google_map import GoogleMap
+from app.parser import Parser
+
+from flask import render_template, request, jsonify, make_response
 from app import my_app
 
 
-@my_app.route("/")
-@my_app.route("/index")
+@my_app.route("/", methods=["GET"])
 def index():
-    user = ["christophe", "Marc"]
-    return render_template("index.html", user=user)
+    return render_template("index.html")
 
 
+@my_app.route("/process", methods=["POST"])
 def process():
-    # return json unify
-    pass
+    req = request.get_json()
+    parser = Parser(req["message"])
+    run_parser = parser.process()
+    google_map = GoogleMap()
+    d = google_map.get_coordinate(run_parser)
+    return make_response(jsonify(d))
+
+
+@my_app.route("/guestbook")
+def guestbook():
+    return render_template("/guestbook.html")
+
+
+@my_app.route("/guestbook/create-entry", methods=["POST"])
+def create_entry():
+
+    req = request.get_json()
+    return make_response(jsonify(req), 200)
